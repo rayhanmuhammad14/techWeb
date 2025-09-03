a1079f3d4f40749bc80db1bbb5ef9c67ecd764ae
Salahuddin Syahrir <rayhan10915@gmail.com>
2025-09-03 10:40:53 +0700

    Update Tampilan 404

--- properti/properti.py
+++ properti/properti.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort
 
 properti = Blueprint('properti',__name__,static_folder='static',
                      template_folder='templates', static_url_path='/static')
@@ -6,3 +6,13 @@ properti = Blueprint('properti',__name__,static_folder='static',
 @properti.route('/')
 def index():
     return render_template('properti.html')
+
+@properti.errorhandler(404)
+def page_not_found(error):
+    return render_template('404Properti.html'), 404
+
+@properti.route('/<path:invalid_path>')
+def page_not_found(invalid_path):
+    # Memicu error 404 secara manual
+    abort(404)
+
